f508a450d7e69fca20474718deaf9db1f82823f5
Whitman Spitzer <whitspitzer@gmail.com>
2021-02-22 13:56:35 -0800

    stable server 1

---
+++ rpzwh_2/pythons/http_server/stable_http_server/stable_http_server1.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import os
+from time import sleep
+import http.server
+import socketserver
+
+host_name = '192.168.0.178'  # Change this to your Raspberry Pi IP address
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Server Starts - %s:%s" % (host_name, PORT))
+    httpd.serve_forever()
+
