a948cf0a9fc15c1b3311e0f6209e1ca965b4d14d
blusky-cloud <sspitzer@pdx.edu>
2021-03-01 13:20:25 -0800

    serial test

--- Remote/pythons/pc_serial_comms/pc_rs485_test.py
+++ Remote/pythons/pc_serial_comms/pc_rs485_test.py
@@ -7,7 +7,7 @@ ser = serial.Serial(port, 115200, timeout=0)
 while True:
     data = ser.read(9999)
     if len(data) > 0:
-        print('Got: ', data)
+        print(data)
 
     sleep(0.5)
     print('not blocked')
--- rpzwh_1/pythons/serial_comms/serial_rs485_test.py
+++ rpzwh_1/pythons/serial_comms/serial_rs485_test.py
@@ -1,8 +1,10 @@
 import serial
+from time import sleep
 #
 port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+print('RASPI UART RS485 TEST')
 
 while True:
-    port.write(b"Say something:")
-    rcv = port.read(10)
-    port.write(b"You sent:" + repr(rcv))
+    port.write(b"Connection Successful, This is the DCM Transmitting")
+    #rcv = port.read(10)
+    #port.write(b"You sent:")
