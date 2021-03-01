034d2455356b6f3b1344a653e31522fccaabb3e8
blusky-cloud <sspitzer@pdx.edu>
2021-03-01 15:05:20 -0800

    serial test

--- rpzwh_1/pythons/serial_comms/raspi_rs485_test.py
+++ rpzwh_1/pythons/serial_comms/raspi_rs485_test.py
@@ -6,8 +6,8 @@ from time import sleep
 #
 msg = b'  dcm contact msg'
 port = '/dev/ttyAMA0'
-conn = serial.Serial(port, baudrate=19200)
-conn.rs485_mode = serial.rs485.RS485Settings(False, True)
+conn = serial.Serial(port, baudrate=19200, timeout=None)
+#conn.rs485_mode = serial.rs485.RS485Settings(False, True)
 
 print('RASPI UART RS485 TEST')
 
@@ -15,7 +15,7 @@ Tx_Enable = LED(18)
 Tx_Enable.on()
 
 while True:
-    rconn.write('test rconn'.encode('utf-8'))
+    conn.write('test rconn'.encode('utf-8'))
     #conn.write(b"Connection Successful, This is the DCM Transmitting")
     #rcv = port.read(10)
     #port.write(b"You sent:")
