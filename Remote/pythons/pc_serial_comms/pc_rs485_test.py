5afe4f850b1e586115406186cf2742677a0d003f
blusky-cloud <sspitzer@pdx.edu>
2021-03-01 13:33:16 -0800

    serial test

--- Remote/pythons/pc_serial_comms/pc_rs485_test.py
+++ Remote/pythons/pc_serial_comms/pc_rs485_test.py
@@ -5,11 +5,12 @@ port = "COM5"
 ser = serial.Serial(port, 115200, timeout=0)
 
 while True:
+    print("  attempting read from laptop ")
     data = ser.read(9999)
     if len(data) > 0:
         print(data)
 
-    sleep(0.5)
+    sleep(4)
     print('not blocked')
 
 ser.close()
