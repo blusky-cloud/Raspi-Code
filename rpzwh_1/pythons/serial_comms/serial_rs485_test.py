ca972ec4f2adcc2a01babf72f41ae4a3b811bbb2
blusky-cloud <sspitzer@pdx.edu>
2021-03-01 12:57:52 -0800

    serial test

--- rpzwh_1/pythons/serial_comms/serial_rs485_test.py
+++ rpzwh_1/pythons/serial_comms/serial_rs485_test.py
@@ -3,6 +3,6 @@ import serial
 port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 
 while True:
-    port.write("\r\nSay something:")
+    port.write("Say something:")
     rcv = port.read(10)
-    port.write("\r\nYou sent:" + repr(rcv))
+    port.write("You sent:" + repr(rcv))
