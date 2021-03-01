f6dbb24b2447014b128a4300134d6927f0a50b9a
blusky-cloud <sspitzer@pdx.edu>
2021-03-01 13:28:51 -0800

    serial test

--- rpzwh_1/pythons/serial_comms/serial_rs485_test.py
+++ rpzwh_1/pythons/serial_comms/serial_rs485_test.py
@@ -5,6 +5,8 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 print('RASPI UART RS485 TEST')
 
 while True:
+    print('attempting port write')
     port.write(b"Connection Successful, This is the DCM Transmitting")
     #rcv = port.read(10)
     #port.write(b"You sent:")
+    sleep(0.25)
