11bf7a50ea01f5455600c2b89e1ec0f59ec5ce25
blusky-cloud <sspitzer@pdx.edu>
2021-03-01 16:59:32 -0800

    serial test

--- Remote/pythons/pc_serial_comms/pc_rs485_test.py
+++ Remote/pythons/pc_serial_comms/pc_rs485_test.py
@@ -2,14 +2,14 @@ import serial
 from time import sleep
 
 port = "COM5"
-ser = serial.Serial(port, 19200, timeout=None)
+ser = serial.Serial(port, 19200, timeout=1)
 
 while True:
     print("  attempting read from laptop ")
     data = ser.read(15)
     if len(data) > 0:
         print(data)
-        ser.write('msg received')
+        #ser.write('msg received')
 
     sleep(4)
     print('not blocked')
