3169745d1410bbe4810f7318e1c7b898252e1383
blusky-cloud <sspitzer@pdx.edu>
2021-03-02 10:50:02 -0800

    serial test

--- Remote/pythons/pc_serial_comms/pc_rs485_test.py
+++ Remote/pythons/pc_serial_comms/pc_rs485_test.py
@@ -9,18 +9,18 @@ print('INITIATING PC TEST')
 while True:
     print('PC receive')
     count = 0
-    while count < 8:
+    while count < 300:
         data = ser.read(15)
         if len(data) > 0:
             print(data)
         count += 1
         print(count)
-        sleep(0.25)
+        sleep(0.01)
     count = 0
     print('PC transmit')
-    while count < 8:
+    while count < 300:
         ser.write('P'.encode('utf-8'))
         count += 1
-        sleep(0.25)
+        sleep(0.01)
 
 ser.close()
