942df76442e1d4809bd139eead12b3e1822ada7c
blusky-cloud <sspitzer@pdx.edu>
2021-03-05 14:04:36 -0800

    stuff

--- Remote/pythons/pc_serial_comms/pc_rs485_2.py
+++ Remote/pythons/pc_serial_comms/pc_rs485_2.py
@@ -18,20 +18,20 @@ while True:
     print('attempting raspi transmit')
     #Tx_Enable.on()
     count = 0
-    while count < 300:
+    while count < 6:
         conn.write('R'.encode('utf-8'))
         count += 1
-        sleep(0.01)
+        sleep(0.5)
 
 #    Tx_Enable.off()
     count = 0
     sleep(0.5)
     print('attempting raspi receive')
-    while count < 300:
+    while count < 6:
         rcv = conn.read(15)
         if len(rcv) > 0:
             print(rcv)
-        sleep(0.01)
+        sleep(0.5)
         count += 1
 
 conn.close()
