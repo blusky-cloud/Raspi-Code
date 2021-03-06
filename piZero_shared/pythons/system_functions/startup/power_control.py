248aaff8af23010aa3e0358cd376fe5ff7e5e24b
blusky-cloud <sspitzer@pdx.edu>
2021-03-06 13:32:47 -0800

    stuff

--- piZero_shared/pythons/system_functions/startup/power_control.py
+++ piZero_shared/pythons/system_functions/startup/power_control.py
@@ -3,7 +3,7 @@ import time
 import os
 import subprocess
 
-def button_callback(channel):
+def button_callback(buttonState):
 	buttonState = True #because the button was pushed to get here
 	flag = 0
 	while buttonState:
@@ -29,9 +29,24 @@ GPIO.setup(ledPin, GPIO.OUT)
 GPIO.output(ledPin, GPIO.LOW)
 time.sleep(1)
 GPIO.output(ledPin, GPIO.HIGH)
+buttonPushed = False
+flag = 0
 
 while True:
-	GPIO.add_event_detect(buttonPin, GPIO.RISING, callback=button_callback)
+	buttonPushed = GPIO.input(buttonPin)
+	flag = 0
+	while buttonPushed:
+		GPIO.output(ledPin, GPIO.LOW)
+		time.sleep(0.25)
+		GPIO.output(ledPin, GPIO.HIGH)
+		buttonPushed = GPIO.input(buttonPin)
+		flag += 1
+		if buttonState and flag > 8:
+			print("SHUTDOWN TEST")
+
+	print("NOT PRESSED LONG ENOUGH")
+
+
 	time.sleep(0.25)
 
 GPIO.cleanup()
